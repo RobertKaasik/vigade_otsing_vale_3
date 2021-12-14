5e57938d9875e81c2a6501009a366dc888975415
Opilane TTHK <opilane@tthk.ee>
2021-12-14 11:24:27 +0200

    Add project files.

---
+++ vigade_otsing_vale_3.py
@@ -0,0 +1,80 @@
+from random import *
+s=pos=neg=nol=[]
+def arvud_loendis():
+    print("Данные:")
+    n=abs(int(input("Сколько целых чисел генерируем в список? => ")))
+    mini=int(input("Введите минимальное число диапазона => "))
+    maxi=int(input("Введите максимальное число диапазона => "))
+    if mini>=maxi:
+        mini,maxi=vahetus(mini,maxi)
+    generaator(n,s,mini,maxi)
+    print()
+    print("Результаты:")
+    print("Полученный список от",mini,"до",maxi,s)
+    s.sort()
+    print("Отсортированный список",s)
+    jagamine(s,pos,neg,nol)
+    print("Список положительных элементов",pos)
+    print("Список отрицательных элементов",neg)
+    print("Список нулевых элементов",nol)
+    kesk=keskmine(pos)
+    lisamine(s,kesk)
+    print("Среднее положительных:",kesk)
+    kesk=keskmine(neg)
+    lisamine(s,kesk)
+    print("Среднее отрицательных:",kesk)
+    print("Добавляем средние в изначалный массив:")
+    print(s)
+
+def vahetus(a:int,b:int):
+    """Kui min suurem kui max, siis vahetame neid omavahel
+    :param int a: minimaalne arv, mis on suurem kui max
+    :param in b: maximaalne arv, mis on väike kui min
+    :rtype: int,int
+    """
+    abi=a
+    a=b
+    b=abi
+    return a,b
+
+def generaator(n:int,loend:list,a:int,b:int):
+    for i in range (n): 
+        loend.append(randint(a,b))
+    
+
+def jagamine(loend:list,p:list,n:list,nol:list):
+    """
+    :rtype list
+    """
+    for el in loend:
+        if el>0:
+            p.append(el)
+        elif el<0:
+            n.append(el)
+        else:
+            nol.append(el)
+
+def keskmine(loend):
+    """
+    :rtype int
+    """
+    n=len(loend)
+    if n==0:
+        kesk=0
+    else:
+        sum=0
+        for i in loend:
+            sum+=i
+        kesk=round(sum/n,2)
+    return kesk
+
+def lisamine(loend:list,el:float):
+    """
+    :rtype float
+    """
+    loend.append(el)
+    loend.sort()
+
+arvud_loendis()
+    
+
